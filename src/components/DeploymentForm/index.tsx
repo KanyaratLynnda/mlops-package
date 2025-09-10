2bd94f874e43bb95d93c6ab5d5b6b1bd30eae413
Kanyarat Lee <kanyaratlee@Kanyarats-MacBook-Pro.local>
2025-09-09 18:50:26 -0700

    change color

--- src/components/DeploymentForm/index.tsx
+++ src/components/DeploymentForm/index.tsx
@@ -45,7 +45,7 @@ function FormContent() {
   };
 
   return (
-    <div className="min-h-screen py-8" style={{ backgroundColor: 'rgb(220, 150, 160)' }}>
+    <div className="min-h-screen py-8" style={{ backgroundColor: 'rgba(231, 200, 205, 1)' }}>
       <div className="max-w-4xl mx-auto px-4">
         <ProgressHeader />
 
