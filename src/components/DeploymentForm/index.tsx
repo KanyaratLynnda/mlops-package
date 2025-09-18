ef07bc0a8972f15c5e23242009c484f24b25a5ae
Kanyarat Lee <kanyaratlee@mac.mynetworksettings.com>
2025-09-18 14:42:02 -0400

    add recent input

--- src/components/DeploymentForm/index.tsx
+++ src/components/DeploymentForm/index.tsx
@@ -45,7 +45,7 @@ function FormContent() {
   };
 
   return (
-    <div className="min-h-screen py-8" style={{ backgroundColor: 'rgba(231, 200, 205, 1)' }}>
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 py-8">
       <div className="max-w-4xl mx-auto px-4">
         <ProgressHeader />
 
